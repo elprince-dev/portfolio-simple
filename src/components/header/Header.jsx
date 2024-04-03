769cbb6aadd2e85a0309928414b5b05d6d2c0cbf
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-04-03 16:17:34 -0400

    Sets the basic structure of the header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import './header.scss'
+
+const Header = () => {
+
+    const links = ["Home", "About", "Projects", "Contact"]
+  return (
+    <div className="container">
+        <div className="name">
+            <h3>El Prince</h3>
+        </div>
+        <div className="navbar">
+            {links.map(link =>(
+                <a key={link}>{link}</a>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Header
