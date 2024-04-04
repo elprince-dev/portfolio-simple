7cbb29960b9f3681504136feabc69a76cf2fd866
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-04-04 06:25:08 -0400

    Sets the structure of the intro section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@@/components/header/Header";
+import Intro from "@@/components/intro/Intro";
 
 export default function Home() {
   return (
     <div className="main">
       <Header />
+      <Intro />
     </div>
   );
 }
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,26 @@
+import './intro.scss'
+
+const Intro = () => {
+  return (
+    <div className='container'>
+        <div className="name">
+            <h1>Mohammad El Prince</h1>
+        </div>
+
+        <div className="line"></div>
+
+        <div className="text">
+            <p>
+            A Full Stack Developer with passion for problem-solving and collaboration. Equipped with expertise in JavaScript, Python, and a constant drive for continuous learning, Let's connect and share information and ideas!
+            </p>
+        </div>
+
+        <div className="bottons">
+            <button>Projects</button>
+            <button>Contact</button>
+        </div>
+    </div>
+  )
+}
+
+export default Intro
