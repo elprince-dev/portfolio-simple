edce17f9e139b9e68c9a2aacb122c939c798488c
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-04-08 23:11:05 -0400

    Adds the tech stack to the about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,8 @@
 import './about.scss'
 
 const About = () => {
+
+  const skills = ['html', 'css', 'javascript', 'react','nextjs', 'python', 'flask', 'git', 'sqlite', 'scss']
   return (
     <div className="about">
         <h1>About</h1>
@@ -9,7 +11,10 @@ const About = () => {
         Full Stack Software Engineer with experience in JavaScript and Python based programming. With a background in mechanical engineering and operations, I have a can-do attitude and bring strong skills in problem-solving, communication and collaboration. As a lifelong learner, I continuously work on growing my technical knowledge. I enjoy tackling challenges head-on and crafting creative solutions to real-world problems. My belief in the power of teamwork guides me in achieving exceptional results.
         </p>
         <div className="techStack">
-
+          <h4>Tech Stack <br /></h4>
+          {skills.map(tech => (
+                  <img key={tech} src={`/${tech}.svg`} alt='' />
+                ))}
         </div>
     </div>
   )
