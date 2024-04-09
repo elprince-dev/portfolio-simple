0d21b2cb15f68cd190c4eb6854ac806639dd7e85
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-04-08 22:56:04 -0400

    Sets up the styling of the message section and the form submission function

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,30 @@
 'use client'
 import './contact.scss'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
 
     const [error, setError] = useState(false)
     const [success, setSuccess] = useState(false)
+    const formRef = useRef()
+
+    function sendEmail(e){
+        e.preventDefault();
+    
+        emailjs
+          .sendForm('service_qes66hs', 'template_uxvoebn', formRef.current, {
+            publicKey: 'XHM38tfM4cFjaIgaE',
+          })
+          .then(
+            () => {
+              setSuccess(true)
+            },
+            (error) => {
+                setError(true)
+            },
+          );
+    }
 
   return (
     <div className='contact'>
@@ -15,26 +34,26 @@ const Contact = () => {
 
         <div className="line"></div>
 
-        <form>
+        <form ref={formRef} onSubmit={sendEmail}>
             <div className='nameEmail'>
-                <label>
-                    Name: <br /></label>
-                    <input type="text" required placeholder='Name' name='user_name' placeholder ="Your name" />
+                <label>Name: <br /></label>
+                <input type="text" required placeholder='Name' name='user_name' placeholder ="Your name" />
                 
-                <label>
-                    Email Address: <br /></label>
-                    <input type="email" required placeholder='Email' name='user_email' placeholder ="Email address" />
+                <label>Email Address: <br /></label>
+                <input type="email" required placeholder='Email' name='user_email' placeholder ="Email address" />
                 
             </div>
             
             <div className='textArea'>
-            <label>
-                    Your Message: <br /></label>
-                <textarea rows="8" placeholder='Message' name='message' rows = '5'/>
+                <label>Your Message: <br /></label>
+                <textarea name='message' rows = '5' placeholder = 'Your Message'/>
             </div>
             <button>Submit</button>
-            {error && 'Error! Please try again'}
+            <div className='error'>
+                {error && 'Error! Please try again'}
             {success && 'Thank you! I will get back to you so soon!'}
+            </div>
+            
         </form>
     </div>
   )
