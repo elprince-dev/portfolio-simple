4682b69fcb43d22912e8e5c3fbe16260d88819ec
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-04-21 11:30:17 -0400

    Makes responsive navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,30 @@
+'use client'
+import { useRef } from 'react';
 import './header.scss'
-
+import { FaBars, FaTimes } from "react-icons/fa";
 const Header = () => {
 
     const links = ["Home", "About", "Projects", "Contact"]
+    const navRef = useRef()
+    const showNavBar = () => {
+        navRef.current.classList.toggle('responsiveNav')
+    }
   return (
     <div className="header">
         <div className="name">
             <h3>El Prince</h3>
         </div>
-        <div className="navbar">
+        <div className="navbar" ref={navRef}>
             {links.map(link =>(
-                <a key={link} className='link' href={`#${link}`}>{link}</a>
+                <a key={link} className='link' href={`#${link}`} onClick={showNavBar}>{link} </a>
             ))}
+            <button onClick={showNavBar} className='navButton navCloseButton'>
+                <FaTimes className='FaTimes'/>
+            </button>
         </div>
+        <button>
+            <FaBars onClick={showNavBar} className='FaBars'/>
+        </button>   
     </div>
   )
 }
